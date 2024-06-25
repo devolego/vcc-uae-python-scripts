055860e4b8b2d1aeed63baa55aad346199517f78
devolego <dev.olegovich@gmail.com>
2024-06-25 23:31:21 +0400

    port changes

--- app.py
+++ app.py
@@ -123,9 +123,5 @@ def preprocess():
 if __name__ == '__main__':
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
